9709b19cfb68111f6486e6f8df67256d29687dbc
BİLTEKİN KURTULUŞ <112376639+serhatbiltekin07@users.noreply.github.com>
2024-02-01 14:08:03 +0300

    Add files via upload

---
+++ ClassMetotDemo/ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class MusteriManager 
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Eklenen Müşteri :"+ musteri.Name+" "+ musteri.LastName);
+            Console.WriteLine("Müşteri numarası: "+ musteri.phone);
+            Console.WriteLine("Eklendi.");
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Eklenen Müşteri :" + musteri.Name + " " + musteri.LastName);
+            Console.WriteLine("Müşteri numarası: " + musteri.phone);
+            Console.WriteLine("Silindi.");
+        }
+         
+        
+        public void List(Musteri musteri)
+        {
+            Console.WriteLine("Eklenen Müşteri :" + musteri.Name + " " + musteri.LastName);
+            Console.WriteLine("Müşteri numarası: " + musteri.phone);
+            Console.WriteLine("Listelendi.");
+        }
+    }
+}
---
+++ ClassMetotDemo/ClassMetotDemo/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace ClassMetotDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Name = "Biltekin";
+            musteri1.LastName = "Kurtuluş";
+            musteri1.phone = "555555555";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Name = "AhmetAli";
+            musteri2.LastName = "Kar";
+            musteri2.phone = "444444444";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 1;
+            musteri3.Name = "Tolga";
+            musteri3.LastName = "Noyan";
+            musteri3.phone = "99999999";
+
+            List<Musteri> musteriler = new List<Musteri> { musteri1, musteri2, musteri3 };
+
+            MusteriManager musteriManager = new MusteriManager();
+            foreach (var item in musteriler)
+            {
+                musteriManager.Ekle(item);
+            }
+            Console.WriteLine("-------------------------------------------------------------");
+            foreach (var item in musteriler)
+            {
+                musteriManager.List(item);
+            }
+
+            Console.WriteLine("-------------------------------------------------------------");
+
+            foreach (var item in musteriler)
+            {
+                musteriManager.Sil(item);
+            }
+        }
+    }
+}
